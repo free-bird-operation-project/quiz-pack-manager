30115443efd692e0b02e6b017f8aead77eb17882
Kyle Angelo Galendez <xcxkyleangelo26@gmail.com>
2024-03-17 21:28:03 +0800

    Update webpack configurations
    - Refactor webpack configurations to improve build process
    - Update output file paths and directory structure
    - Add HtmlWebpackPlugin for generating HTML files
    - Include asset handling for images and fonts
    - Adjust devServer configuration for improved development experience

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,23 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = (env, argv) => ({
 	mode: argv.mode === 'production' ? 'production' : 'development',
 	entry: './src/app.js',
 	output: {
-		filename:
-			argv.mode === 'production' ? 'build/bundle.min.js' : 'build/bundle.js',
-		path: path.resolve(__dirname, 'public')
+		filename: argv.mode === 'production' ? 'bundle.min.js' : 'bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: true
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Quiz Pack Manager'
+		})
+	],
 	module: {
 		rules: [
 			{
-				test: /\.js$/,
+				test: /\.js$/i,
 				exclude: /node_modules/,
 				use: {
 					loader: 'babel-loader',
@@ -21,14 +27,23 @@ module.exports = (env, argv) => ({
 				}
 			},
 			{
-				test: /\.css$/,
+				test: /\.css$/i,
 				exclude: /node_modules/,
 				use: ['style-loader', 'css-loader']
+			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				exclude: /node_modules/,
+				type: 'asset/resource'
+			},
+			{
+				test: /\.(woff|woff2|eot|ttf|otf)$/i,
+				type: 'asset/resource'
 			}
 		]
 	},
 	devServer: {
-		static: path.join(__dirname, 'public'),
+		static: path.join(__dirname, 'dist'),
 		compress: true,
 		port: 8000
 	}
